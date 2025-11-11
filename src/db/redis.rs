89068ab4bb360cea1e4590d93aa8e4b4c4e30716
LaZy志 <18826233662@163.com>
2025-11-11 17:54:06 +0800

    redis

--- src/db/redis.rs
+++ src/db/redis.rs
@@ -1,5 +1,20 @@
-use crate::config::RedisConfig;
 
-pub async fn init(config: &RedisConfig) {
-    
+pub type RedisPool = bb8::Pool<bb8_redis::RedisConnectionManager>;
+pub type RedisConnection<'a> = bb8::PooledConnection<'a, bb8_redis::RedisConnectionManager>;
+
+#[derive(Clone)]
+pub struct RedisClient {
+    pool: RedisPool,
+}
+
+impl RedisClient {
+    pub async fn new() -> RedisResult<Self> {
+        let config = crate::config::get();
+        let redis_config = &config.redis;
+
+        let manager = bb8_redis::RedisConnectionManager::new(redis_config.to_url())?;
+        let pool = bb8::Pool::builder().build(manager).await?;
+
+        Ok(Self { pool })
+    }
 }
